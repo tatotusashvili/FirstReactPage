c51063e3f4b4fa7e3989a678f31384ea2bdcccb6
tatotusashvili <97541821+tatotusashvili@users.noreply.github.com>
2023-03-03 21:38:50 +0400

    react

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+
+function App() {
+  return (
+    <div className="MainDiv">
+      <img className="cube" src="image-equilibrium.jpg" alt="cube but why"/>
+      <h1>Equilibrium #3429</h1>
+      <p>Our Equilibrium collection promotes balance and calm.</p>
+      <div className="price">
+        <svg width="11" height="18" xmlns="http://www.w3.org/2000/svg"> <path d="M11 10.216 5.5 18 0 10.216l5.5 3.263 5.5-3.262ZM5.5 0l5.496 9.169L5.5 12.43 0 9.17 5.5 0Z" fill="#00FFF8"/></svg>
+        <h2>0.041ETH</h2>
+        <svg className="clockIcon" width="17" height="17" xmlns="http://www.w3.org/2000/svg"> <path d="M8.305 2.007a6.667 6.667 0 1 0 0 13.334 6.667 6.667 0 0 0 0-13.334Zm2.667 7.334H8.305a.667.667 0 0 1-.667-.667V6.007a.667.667 0 0 1 1.334 0v2h2a.667.667 0 0 1 0 1.334Z" fill="#8BACD9"/></svg>
+        <h3>3 days left</h3>
+    </div>
+    <div className="line"></div>
+    <div className="footer">
+      <img className="iconImage" src="image-avatar.png" alt="icon"/>
+      <p>creation of</p>
+      <h4>jules Wyvern</h4>
+    </div>
+    </div>
+  );
+}
+
+
+export default App;
